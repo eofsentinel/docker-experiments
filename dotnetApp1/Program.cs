aa74889ec6b7c2befed0ef0bdd20222b27907be3
Abhishek Chakraborty <eofsentinel@outlook.com>
2023-08-12 22:23:00 +0530

    Added dockerfile

--- dotnetApp1/Program.cs
+++ dotnetApp1/Program.cs
@@ -7,6 +7,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.Write("Please enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hi {name}! The current time is {DateTimeOffset.UtcNow} UTC");
         }
     }
 }
